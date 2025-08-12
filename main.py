970c54427aeb7f445356b08f9e792f6a2d9c5f15
allaindm <allaindemoupu@mail.ru>
2025-08-12 22:45:52 +0300

    Класс для обработки отчета.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from aiogram.enums import ParseMode
 
 import config
 import parser
+from report_handler import ReportHandler
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
@@ -89,6 +90,7 @@ async def echo_mess(message: types.Message):
             t_o = "ТО Восток"
 
         # Пересчет даты под запрос.
+        # TODO возможно стоит перенести логику определения даты.
         date_now = datetime.now()
         logger.info(f"Текущая дата: {date_now}")
         date_ago = date_now - timedelta(hours=config.HOUR)  # здесь мы выставляем минус 15 часов
@@ -97,230 +99,45 @@ async def echo_mess(message: types.Message):
         date_now_no_year = date_ago.strftime("%d.%m")
         month_year = date_ago.strftime("%m.%Y")
 
-        # Функция отправки отчета в телеграм по уже собранным данным
+        # Обработка текстовых команд.
+        # Запрос выписок из отчетов с привлеченными
         if message.text.lower() == "привлеченные":
             # Для получения папки месяца привлеченных вычтем 8 дней(максимальный срок, когда они должны быть уже сданы)
             date_ago = date_ago - timedelta(8)
             date_now_year = date_ago.strftime("%d.%m.%Y")
             month_year = date_ago.strftime("%m.%Y")
+
+        # Запрос недельного отчета.
+        # TODO реализовать логику.
         elif message.text.lower() == "неделя":
             # Обработка запроса "неделя"
             pass
+
+        # Запрос отчета, за указанное количество дней назад
         elif message.text.isdigit() and 1 <= int(message.text) <= config.MAX_REPORT_DAYS_AGO:
-            days_to_subtract = int(message.text) - 1
-            date_ago = date_ago - timedelta(days=days_to_subtract)
-            logger.info(f"Новая дата: {date_ago}")
-            date_now_year = date_ago.strftime("%d.%m.%Y")
-            month_year = date_ago.strftime("%m.%Y")
+            # Для получения отчета только авторизованный админ
+            if user_id in config.USERS:
+                days_to_subtract = int(message.text) - 1
+                date_ago = date_ago - timedelta(days=days_to_subtract)
+                logger.info(f"Новая дата: {date_ago}")
+                date_now_year = date_ago.strftime("%d.%m.%Y")
+                month_year = date_ago.strftime("%m.%Y")
 
-        # # Для получения отчета только авторизованный админ
-        # if user_id in config.USERS:
 
-        # Парсер сообщений и сохранение в файл если это отчет.
+        # Обработка текста, для определения отчета мастеров.
         else:
             try:
                 # Создадим папку за текущий день/месяц если не существует
                 if not os.path.exists(f"files/{t_o}/{month_year}/{date_now_year}"):
                     os.makedirs(f"files/{t_o}/{month_year}/{date_now_year}")
-                await parser_report(t_o, message)
+                # await parser_report(t_o, message)
+                report = ReportHandler(message, t_o, date_now_year, month_year)
+                await report.process_report()
+
             except IndexError:
                 logger.info("Тут видимо сообщение не относящееся к отчету.")
 
 
-async def parser_report(t_o, message):
-
-    et_int = 0
-    et_int_pri = 0
-    et_tv = 0
-    et_tv_pri = 0
-    et_dom = 0
-    et_dom_pri = 0
-    et_serv = 0
-    et_serv_tv = 0
-
-    # Создадим флаги для поиска ошибок
-    et_int_flag = 0
-    et_int_pri_flag = 0
-    et_tv_flag = 0
-    et_tv_pri_flag = 0
-    et_dom_flag = 0
-    et_dom_pri_flag = 0
-    et_serv_flag = 0
-    et_serv_tv_flag = 0
-
-    # TODO Разбивка по ":" старый способ, по нему определялся провайдер.
-    # TODO Добавить проверку если будут проблемы
-    # Разбиваем по ":", так мы определим что это отчет.
-    pre_txt_lower = message.text.lower()
-    logger.info(f"pre_txt_lower {pre_txt_lower}")
-    # Мастера могут добавлять лишние ":" при перечислении.
-    pre_txt = (pre_txt_lower.replace("тв:", "тв").
-               replace("ис:", "ис").
-               replace("нет:", "нет").
-               replace("он:", "он"))
-    logger.info(f"pre_txt {pre_txt}")
-    txt = pre_txt.split(":")
-    logger.info(f"txt {txt}")
-
-    # Заменим скобки и перенос строки пробелами и разобьем на список
-    new_txt = (txt[1].replace("(", " ").
-                   replace(")", " ").
-                   replace("\n", " ").
-                   replace(",", " ").
-                   replace(":", "").
-                   replace(";", "").
-                   replace("\xa0", " ").
-                   replace(".", " "))
-    new_txt_list_with_space = new_txt.split(" ")
-    new_txt_list = [i for i in new_txt_list_with_space if i]
-
-    for num, val in enumerate(new_txt_list):
-        if val.lower() == "интернет" and new_txt_list[num - 1].lower() != "сервис":
-            try:
-                et_int = int(new_txt_list.pop(num + 1))  # Следующее значение после "интернет"
-                if et_int < 100:  # Проверка на длину значения, защита от номера сервиса
-                    et_int_flag = 1  # Флаг для проверки правильности отчета
-            except ValueError:
-                et_int = 0
-            # logger.info(new_txt_list)
-
-    # Сочетание тв
-    for num, val in enumerate(new_txt_list):
-        if val.lower() == "тв":
-            if new_txt_list[num - 1].lower() == "сервис":
-                try:
-                    et_serv_tv = int(new_txt_list.pop(num + 1))  # После "тв"
-                    if et_serv_tv < 100:  # Проверка на длину значения, защита от номера сервиса
-                        et_serv_tv_flag = 1  # Флаг для проверки правильности отчета
-                except ValueError:
-                    et_serv_tv = 0
-                except IndexError:  # После сервисов тв часто не ставят значение, а это конец сообщения
-                    et_serv_tv = 0
-                # logger.info(new_txt_list)
-
-    for num, val in enumerate(new_txt_list):
-        if val.lower() == "тв":
-            if new_txt_list[num - 1].lower() != "сервис":
-                try:
-                    et_tv = int(new_txt_list.pop(num + 1))  # После "тв"
-                    if et_tv < 100:  # Проверка на длину значения, защита от номера сервиса
-                        et_tv_flag = 1  # Флаг для проверки правильности отчета
-                except ValueError:
-                    et_tv = 0
-                except IndexError:  # После сервисов тв часто не ставят значение, а это конец сообщения
-                    et_tv = 0
-                # logger.info(new_txt_list)
-    # Домофон
-    for num, val in enumerate(new_txt_list):
-        if val.lower() == "домофон":
-            try:
-                et_dom = int(new_txt_list.pop(num + 1))  # После "домофон"
-                if et_dom < 100:  # Проверка на длину значения, защита от номера сервиса
-                    et_dom_flag = 1  # Флаг для проверки правильности отчета
-            except ValueError:
-                et_dom = 0
-            # logger.info(new_txt_list)
-
-    # Сервис интернет
-    for num, val in enumerate(new_txt_list):
-        if val.lower() == "сервис" and new_txt_list[num + 1].lower() == "интернет":
-            try:
-                et_serv = int(new_txt_list.pop(num + 2))  # + 2 ибо через слово "интернет"
-                if et_serv < 100:  # Проверка на длину значения, защита от номера сервиса
-                    et_serv_flag = 1  # Флаг для проверки правильности отчета
-            except ValueError:
-                et_serv = 0
-            # logger.info(new_txt_list)
-
-    flag_priv_int = 0
-    flag_priv_tv = 0
-    flag_priv_dom = 0
-
-    # Привлеченные
-    for num, val in enumerate(new_txt_list):
-        if val[0:4].lower() == "прив":
-            if flag_priv_int == 0:  # Флаг привлеченного интернета
-                # logger.info(f"тут привлеченный интернет {new_txt_list[num - 1]}")
-                flag_priv_int += 1
-                try:
-                    et_int_pri = int(new_txt_list[num - 1])  # Перед "прив"
-                    if et_int_pri < 100:  # Проверка на длину значения, защита от номера сервиса
-                        et_int_pri_flag = 1  # Флаг для проверки правильности отчета
-                except ValueError:
-                    et_int_pri = 0
-            elif flag_priv_tv == 0:  # Флаг привлеченного тв
-                # logger.info(f"тут привлеченный тв {new_txt_list[num - 1]}")
-                flag_priv_tv += 1
-                try:
-                    et_tv_pri = int(new_txt_list[num - 1])  # Перед "прив"
-                    if et_tv_pri < 100:  # Проверка на длину значения, защита от номера сервиса
-                        et_tv_pri_flag = 1  # Флаг для проверки правильности отчета
-                except ValueError:
-                    et_tv_pri = 0
-            elif flag_priv_dom == 0:  # Флаг привлеченного домофона
-                # logger.info(f"тут привлеченный домофон {new_txt_list[num - 1]}")
-                flag_priv_dom += 1
-                try:
-                    et_dom_pri = int(new_txt_list[num - 1])  # Перед "прив"
-                    if et_dom_pri < 100:  # Проверка на длину значения, защита от номера сервиса
-                        et_dom_pri_flag = 1  # Флаг для проверки правильности отчета
-                except ValueError:
-                    et_dom_pri = 0
-
-    to_save = {
-        "et_int": et_int,
-        "et_int_pri": et_int_pri,
-        "et_tv": et_tv,
-        "et_tv_pri": et_tv_pri,
-        "et_dom": et_dom,
-        "et_dom_pri": et_dom_pri,
-        "et_serv": et_serv,
-        "et_serv_tv": et_serv_tv,
-        'master': "не указан",
-        'msg_err_txt': ""  # Запись текста с возможными ошибками
-    }
-    logger.info(f"Для сохранения: {to_save}")
-
-    # Если в начале сообщения есть фамилия, то возьмем ее.
-    txt_soname_pre = txt[0].replace("\n", " ")
-    txt_soname = txt_soname_pre.split(" ")
-    if txt_soname[0][0:2].lower() != 'ет':
-        if txt_soname[0][0:2].lower() == "то":
-            await message.reply("Необходимо указать фамилию мастера, отчет не сохранен.")
-            return
-        else:
-            to_save["master"] = txt_soname[0].title()
-
-    if to_save["master"] == "не указан":
-        await message.reply("Необходимо указать фамилию мастера, отчет не сохранен.")
-        return
-
-    # Сообщение об ошибке на основе флагов
-    msg_err = []
-    if et_int_flag == 0:
-        msg_err.append("ЕТ интернет. ")
-    if et_int_pri_flag == 0:
-        msg_err.append("ЕТ интернет. ")  # привлеченный
-    if et_tv_flag == 0:
-        msg_err.append("ЕТ тв. ")
-    if et_tv_pri_flag == 0:
-        msg_err.append("ЕТ тв. ")  # привлеченный
-    if et_dom_flag == 0:
-        msg_err.append("ЕТ домофон. ")
-    if et_dom_pri_flag == 0:
-        msg_err.append("ЕТ домофон. ")  # привлеченный
-    if et_serv_flag == 0:
-        msg_err.append("ЕТ сервис. ")
-    if et_serv_tv_flag == 0:
-        msg_err.append("ЕТ сервис тв. ")
-
-    if len(msg_err) > 0:
-        msg_err_txt = f""
-        for e in msg_err:
-            msg_err_txt += e
-        await message.reply(f"Внимание, возможна ошибка с отчетом мастера "
-                            f"{to_save['master']}: {msg_err_txt} Отчет не сохранен.")
-        return
 
 # Основная функция запуска бота
 async def main():
---
+++ report_handler.py
@@ -0,0 +1,280 @@
+
+import json
+import logging
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Класс отчета мастера
+class ReportHandler:
+    def __init__(self, message, t_o, date_now_year, month_year):
+        self.message = message  # Сообщение из ТГ
+        self.main_txt = ""      # Разобранное сообщения для обработки парсером
+        self.t_o = t_o  # Территориальное подразделение
+        self.date_now_year = date_now_year  # Обсчитанная дата с годом
+        self.month_year = month_year        # Обсчитанная дата без года
+
+        # Счетчик количества сделанных заявок
+        # self.counters = {
+        #     "et_int": 0, "et_int_pri": 0, "et_tv": 0,
+        #     "et_tv_pri": 0, "et_dom": 0, "et_dom_pri": 0,
+        #     "et_serv": 0, "et_serv_tv": 0
+        # }
+        self.et_int = 0
+        self.et_int_pri = 0
+        self.et_tv = 0
+        self.et_tv_pri = 0
+        self.et_dom = 0
+        self.et_dom_pri = 0
+        self.et_serv = 0
+        self.et_serv_tv = 0
+
+        # Флаги для поиска ошибок. 0 == ошибка.
+        # self.flags = {
+        #     "et_int": 0, "et_int_pri": 0, "et_tv": 0,
+        #     "et_tv_pri": 0, "et_dom": 0, "et_dom_pri": 0,
+        #     "et_serv": 0, "et_serv_tv": 0
+        # }
+        self.et_int_flag = 0
+        self.et_int_pri_flag = 0
+        self.et_tv_flag = 0
+        self.et_tv_pri_flag = 0
+        self.et_dom_flag = 0
+        self.et_dom_pri_flag = 0
+        self.et_serv_flag = 0
+        self.et_serv_tv_flag = 0
+
+        # Фамилия мастера для сохранения отчета
+        self.master = "не указан"
+        # Список указанных мастером номеров сервисных заявок(все кроме ЛС подключений)
+        self.list_repairs = []
+
+    # Запуск всех методов для обработки отчета
+    async def process_report(self):
+        await self._parse_message()
+        await self._validate_master()
+        await self._collect_repair_numbers()
+        await self._save_report()
+
+    # Обработка отчета для получения количества выполненных заявок
+    async def _parse_message(self):
+        # TODO Разбивка по ":" старый способ, по нему определялся провайдер.
+        # TODO Добавить проверку если будут проблемы
+        # Разбиваем по ":", так мы определим что это отчет.
+        pre_txt_lower = self.message.text.lower()
+        logger.info(f"pre_txt_lower {pre_txt_lower}")
+        # Мастера могут добавлять лишние ":" при перечислении.
+        pre_txt = (pre_txt_lower.replace("тв:", "тв").
+                   replace("ис:", "ис").
+                   replace("нет:", "нет").
+                   replace("он:", "он"))
+        logger.info(f"pre_txt {pre_txt}")
+        self.main_txt = pre_txt.split(":")
+        logger.info(f"self.main_txt {self.main_txt}")
+
+        # Заменим скобки и перенос строки пробелами и разобьем на список
+        # new_txt = (txt.replace("(", " ").
+        new_txt = (self.main_txt[1].replace("(", " ").
+                   replace(")", " ").
+                   replace("\n", " ").
+                   replace(",", " ").
+                   replace(":", "").
+                   replace(";", "").
+                   replace("\xa0", " ").
+                   replace(".", " "))
+        new_txt_list_with_space = new_txt.split(" ")
+        new_txt_list = [i for i in new_txt_list_with_space if i]
+
+        # Интернет
+        for num, val in enumerate(new_txt_list):
+            if val.lower() == "интернет" and new_txt_list[num - 1].lower() != "сервис":
+                try:
+                    self.et_int = int(new_txt_list.pop(num + 1))  # Следующее значение после "интернет"
+                    if self.et_int < 100:  # Проверка на длину значения, защита от номера сервиса
+                        self.et_int_flag = 1  # Флаг для проверки правильности отчета
+                except ValueError:
+                    self.et_int = 0
+                # logger.info(new_txt_list)
+
+        # Сервис тв
+        for num, val in enumerate(new_txt_list):
+            if val.lower() == "тв":
+                if new_txt_list[num - 1].lower() == "сервис":
+                    try:
+                        self.et_serv_tv = int(new_txt_list.pop(num + 1))  # После "тв"
+                        if self.et_serv_tv < 100:  # Проверка на длину значения, защита от номера сервиса
+                            self.et_serv_tv_flag = 1  # Флаг для проверки правильности отчета
+                    except ValueError:
+                        self.et_serv_tv = 0
+                    except IndexError:  # После сервисов тв часто не ставят значение, а это конец сообщения
+                        self.et_serv_tv = 0
+                    # logger.info(new_txt_list)
+
+        # ТВ
+        for num, val in enumerate(new_txt_list):
+            if val.lower() == "тв":
+                if new_txt_list[num - 1].lower() != "сервис":
+                    try:
+                        self.et_tv = int(new_txt_list.pop(num + 1))  # После "тв"
+                        if self.et_tv < 100:  # Проверка на длину значения, защита от номера сервиса
+                            self.et_tv_flag = 1  # Флаг для проверки правильности отчета
+                    except ValueError:
+                        self.et_tv = 0
+                    except IndexError:  # После сервисов тв часто не ставят значение, а это конец сообщения
+                        self.et_tv = 0
+                    # logger.info(new_txt_list)
+        # Домофон
+        for num, val in enumerate(new_txt_list):
+            if val.lower() == "домофон":
+                try:
+                    self.et_dom = int(new_txt_list.pop(num + 1))  # После "домофон"
+                    if self.et_dom < 100:  # Проверка на длину значения, защита от номера сервиса
+                        self.et_dom_flag = 1  # Флаг для проверки правильности отчета
+                except ValueError:
+                    self.et_dom = 0
+                # logger.info(new_txt_list)
+
+        # Сервис интернет
+        for num, val in enumerate(new_txt_list):
+            if val.lower() == "сервис" and new_txt_list[num + 1].lower() == "интернет":
+                try:
+                    self.et_serv = int(new_txt_list.pop(num + 2))  # + 2 ибо через слово "интернет"
+                    if self.et_serv < 100:  # Проверка на длину значения, защита от номера сервиса
+                        self.et_serv_flag = 1  # Флаг для проверки правильности отчета
+                except ValueError:
+                    self.et_serv = 0
+                # logger.info(new_txt_list)
+
+        # Вычисление привлеченных, а так же поиск ошибки отсутствия нужного количества слов "прив" в отчете.
+        # Перебор отчета, первый привлеченный идет в интернет, второй в тв, третий в домофон.
+        # Флаги для поиска ошибки отсутствия одного из привлеченных.
+        flag_priv_int = 0
+        flag_priv_tv = 0
+        flag_priv_dom = 0
+        for num, val in enumerate(new_txt_list):
+            if val[0:4].lower() == "прив":
+                if flag_priv_int == 0:  # Флаг привлеченного интернета
+                    # logger.info(f"тут привлеченный интернет {new_txt_list[num - 1]}")
+                    flag_priv_int += 1
+                    try:
+                        self.et_int_pri = int(new_txt_list[num - 1])  # Перед "прив"
+                        if self.et_int_pri < 100:  # Проверка на длину значения, защита от номера сервиса
+                            self.et_int_pri_flag = 1  # Флаг для проверки правильности отчета
+                    except ValueError:
+                        self.et_int_pri = 0
+                elif flag_priv_tv == 0:  # Флаг привлеченного тв
+                    # logger.info(f"тут привлеченный тв {new_txt_list[num - 1]}")
+                    flag_priv_tv += 1
+                    try:
+                        self.et_tv_pri = int(new_txt_list[num - 1])  # Перед "прив"
+                        if self.et_tv_pri < 100:  # Проверка на длину значения, защита от номера сервиса
+                            self.et_tv_pri_flag = 1  # Флаг для проверки правильности отчета
+                    except ValueError:
+                        self.et_tv_pri = 0
+                elif flag_priv_dom == 0:  # Флаг привлеченного домофона
+                    # logger.info(f"тут привлеченный домофон {new_txt_list[num - 1]}")
+                    flag_priv_dom += 1
+                    try:
+                        self.et_dom_pri = int(new_txt_list[num - 1])  # Перед "прив"
+                        if self.et_dom_pri < 100:  # Проверка на длину значения, защита от номера сервиса
+                            self.et_dom_pri_flag = 1  # Флаг для проверки правильности отчета
+                    except ValueError:
+                        self.et_dom_pri = 0
+
+        # to_save = {
+        #     "et_int": self.et_int,
+        #     "et_int_pri": self.et_int_pri,
+        #     "et_tv": self.et_tv,
+        #     "et_tv_pri": self.et_tv_pri,
+        #     "et_dom": self.et_dom,
+        #     "et_dom_pri": self.et_dom_pri,
+        #     "et_serv": self.et_serv,
+        #     "et_serv_tv": self.et_serv_tv,
+        #     'master': "не указан",
+        #     'msg_err_txt': ""  # Запись текста с возможными ошибками
+        # }
+        # logger.info(f"Для сохранения: {to_save}")
+
+        # Если в начале сообщения есть фамилия, то возьмем ее.
+        txt_soname_pre = self.main_txt[0].replace("\n", " ")
+        txt_soname = txt_soname_pre.split(" ")
+        if txt_soname[0][0:2].lower() != 'ет':
+            if txt_soname[0][0:2].lower() == "то":
+                await self.message.reply("Необходимо указать фамилию мастера, отчет не сохранен.")
+                return
+            else:
+                self.master = txt_soname[0].title()
+
+        if self.master == "не указан":
+            await self.message.reply("Необходимо указать фамилию мастера, отчет не сохранен.")
+            return
+
+        # Сообщение об ошибке на основе флагов
+        msg_err = []
+        if self.et_int_flag == 0:
+            msg_err.append("ЕТ интернет. ")
+        if self.et_int_pri_flag == 0:
+            msg_err.append("ЕТ привлеченный интернет. ")  # привлеченный
+        if self.et_tv_flag == 0:
+            msg_err.append("ЕТ тв. ")
+        if self.et_tv_pri_flag == 0:
+            msg_err.append("ЕТ привлеченный тв. ")  # привлеченный
+        if self.et_dom_flag == 0:
+            msg_err.append("ЕТ домофон. ")
+        if self.et_dom_pri_flag == 0:
+            msg_err.append("ЕТ привлеченный домофон. ")  # привлеченный
+        if self.et_serv_flag == 0:
+            msg_err.append("ЕТ сервис. ")
+        if self.et_serv_tv_flag == 0:
+            msg_err.append("ЕТ сервис тв. ")
+
+        if len(msg_err) > 0:
+            msg_err_txt = f""
+            for e in msg_err:
+                msg_err_txt += e
+            await self.message.reply(f"Внимание, возможна ошибка с отчетом мастера "
+                                f"{self.master}: {msg_err_txt} Отчет не сохранен.")
+            return
+
+    async def _validate_master(self):
+        pass
+
+    async def _collect_repair_numbers(self):
+        repairs_txt_et = (self.main_txt[1].replace("(", " ").
+                          replace(")", " ").
+                          replace("\n", " ").
+                          replace("#", " ").
+                          replace("e", " ").  # Английская. Тут мастера могут записать етм
+                          replace("е", " ").  # Русская
+
+                          # Для обозначения актовых и без актовых
+                          replace("a", " ").  # Английская
+                          replace("а", " ").  # Русская
+                          replace("б", " ").  # Русская
+                          replace("t", " ").  # Английская
+                          replace("т", " ").  # Русская
+
+                          replace(";", " ").
+                          replace("-", " ").
+                          replace(",", " ").
+                          replace("\xa0", " ").
+                          replace(".", " "))
+
+        repairs_txt_et_list = repairs_txt_et.split(" ")
+        print(f"repairs_txt_et {repairs_txt_et}")
+
+        for i in repairs_txt_et_list:
+            if len(i) == 7 and i.isnumeric():
+                self.list_repairs.append(['ЕТ', i, self.master])
+
+
+    # Сохранение отчета
+    async def _save_report(self):
+        # data = {**self.counters, "master": self.master, "list_repairs": self.list_repairs}
+        data = {
+
+        }
+        with open(f'files/{self.t_o}/{self.month_year}/{self.date_now_year}/{self.master}.json', 'w') as f:
+            json.dump(data, f)
+
